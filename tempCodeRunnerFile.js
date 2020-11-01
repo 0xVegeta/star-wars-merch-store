c2a220118c6e5e3be8c2e530f8110c87de1f9df0
Anas <61079117+Kadastross@users.noreply.github.com>
2020-11-01 22:40:48 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+// const carouselSlide=document.querySelector('.carousel-slide');
+// constt caarouselImages=document.querySelectorAll('.carousel-slide img');
+
+// //buttons
+// const prevBtn = document.querySelector('#prevBtn');
+// const nextBtn = document.querySelector('#nextBtn');
+
+// //counter
+// let counter = 1;
+// const size =carouselImages[0].clientWidth;
+
+// caarouselSlide.style.transform = 'translateX(' + (size*counter)+ 'px)';
+
+function callback(val)
+{
+    console.log(val);
+
+}
